e509a1dd4afb0eafe3e53ce4da3a6fb4f10c67cf
Trevor Mearns <spiral272@gmail.com>
2021-06-01 15:16:03 +0900

    issues with decimal functionality

--- calculator.js
+++ calculator.js
@@ -1,124 +1,129 @@
-const numbers = document.getElementsByClassName('number')
-const operators = document.getElementsByClassName('operator')
-const decimal = document.getElementsByClassName('dot')
-const clearBtn = document.getElementById('clearBtn').addEventListener('click', reset)
-const display = document.getElementById('display')
-const equalsButton = document.querySelector('.equals')
-display.value ="0"
+const numbers = document.querySelectorAll('.number');
+const operators = document.getElementsByClassName('operator');
+const decimalBtn = document.querySelector('.dot');
+const clearBtn = document
+  .getElementById('clearBtn')
+  .addEventListener('click', reset);
+const display = document.getElementById('display');
+const equalsButton = document.querySelector('.equals');
+display.value = '0';
+
+console.log('numbers: ', numbers);
 
 let firstOperand = '';
 let secondOperand = '';
 let operatorValue = '';
-let outcome;
+let outcome = 0;
 let decimalNumber = '.';
 
-function reset(e){
+function reset(e) {
   e.preventDefault();
-  display.value ="0"
+  display.value = '0';
   firstOperand = '';
   secondOperand = '';
   operatorValue = '';
 }
 
-for (i=0; i < decimal.length; i++){
-  const decimalNum = decimal[i];
-  decimalNum.addEventListener('click',function(e){
-    e.preventDefault();
+decimalBtn.addEventListener('click', function (e) {
+  e.preventDefault();
 
-    if (!decimalNumber){
-      
-    }
+  //check if firstOperand or secondOperand contains a decimal, if it does, disable the decimal button
 
-    if (!firstOperand){
-      firstOperand = 0. + Number(firstOperand);
-      console.log('firstOperand is: ', firstOperand)
-    }; 
+  if (!operatorValue) {
+    firstOperand = handleDecimal(firstOperand);
+    display.value = firstOperand;
+  } else if (firstOperand && operatorValue) {
+    secondOperand = handleDecimal(secondOperand);
+    display.value = secondOperand;
+  }
+});
 
-    if (firstOperand && operatorValue){
-      secondOperand = 0. + Number(secondOperand);
-      console.log('secondOperand is:', secondOperand)
-    };
-
-  });
-};
-
-
-for (let i=0; i < numbers.length; i++){
+for (let i = 0; i < numbers.length; i++) {
   const number = numbers[i];
-  number.addEventListener('click', function(e) {
+
+  number.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if (!firstOperand){  
-    //if there isn't 1st operand 
-    //and number button/element was pressed then-->
-      firstOperand = number.textContent;
-      console.log('firstOperand is: ', firstOperand)
-
+    if (!operatorValue) {
+      //if there isn't 1st operand
+      //and number button/element was pressed then-->
+      firstOperand += number.textContent;
+      // if (firstOperand && !operatorValue) {
+      //   firstOperand += firstOperand;
+      // }
+      display.value = firstOperand;
+      console.log('firstOperand is: ', firstOperand);
     }
     if (firstOperand && operatorValue) {
-      secondOperand = number.textContent;
-      console.log('secondOperand is:', secondOperand)
-    };
-    display.value = number.textContent;
-
+      secondOperand += number.textContent;
+      console.log('secondOperand is:', secondOperand);
+    }
+    // display.value = secondOperand;
   });
-};
+}
 
-for (let i=0; i < operators.length; i++){
+function handleDecimal(operand) {
+  const numberArray = operand.split('');
+  if (numberArray.indexOf('.') === -1) {
+    numberArray.push('.');
+  }
+  return numberArray.join('');
+}
+
+for (let i = 0; i < operators.length; i++) {
   const operator = operators[i];
 
-  operator.addEventListener('click', function(e){
+  operator.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if (firstOperand && !operatorValue){
-      //if there was 1st operand and was NOT operator
-      // and operator button was pressed then -->
-      operatorValue = operator.textContent;
-    };
-
-    if (firstOperand && operatorValue && secondOperand){ 
+    if (firstOperand && operatorValue && secondOperand) {
       // continuous functionality/operations.
       //if there were 1st Operand && 2nd Operand && operator
       // and operator button was pressed then -->
-      outcome = calculate();
-      display.value = outcome;
-      firstOperand = outcome;
-      secondOperand = ''
-    };
+      result = calculate(firstOperand, operatorValue, secondOperand);
+      display.value = result;
+      firstOperand = result;
+      operatorValue = operator.textContent;
+      secondOperand = '';
+
+      //console.log('operatorValue: ', operatorValue);
+    } else if (firstOperand) {
+      //if there was 1st operand and was NOT operator
+      // and operator button was pressed then -->
+      operatorValue = operator.textContent;
+    }
+    console.log('operatorValue: ', operatorValue);
   });
-  
-};
+}
 
-
-function calculate(){
-  if (operatorValue === '+'){
+function calculate(firstOperand, operatorValue, secondOperand) {
+  if (operatorValue === '+') {
     outcome = Number(firstOperand) + Number(secondOperand);
-    console.log('the outcome is: ',outcome)
+    console.log('the outcome is: ', outcome);
     return outcome;
-  }; 
+  }
 
-  if (operatorValue === '-'){
+  if (operatorValue === '-') {
     outcome = Number(firstOperand) - Number(secondOperand);
-    return outcome
-  };
-  
-  if (operatorValue === '/'){
+    return outcome;
+  }
+
+  if (operatorValue === '/') {
     outcome = Number(firstOperand) / Number(secondOperand);
-    return outcome
-  };
+    return outcome;
+  }
 
-  if (operatorValue === 'x'){
+  if (operatorValue === 'x') {
     outcome = Number(firstOperand) * Number(secondOperand);
-    return outcome
-  };
-};
+    return outcome;
+  }
+}
 
-equalsButton.addEventListener('click', ()=>{
+equalsButton.addEventListener('click', () => {
   //What condidtions to check before calculating?
-  if(firstOperand && operatorValue && secondOperand){
-    display.value = calculate();
+  if (firstOperand && operatorValue && secondOperand) {
+    display.value = calculate(firstOperand, operatorValue, secondOperand);
     firstOperand = outcome;
-    secondOperand = ''
-  };
+    secondOperand = '';
+  }
 });
-
